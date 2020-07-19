a8de9f48344d2a112b1f8d871f5dd638bc7746f8
Viraj Patidar <vrj381@gmail.com>
2020-07-19 15:09:54 +0530

    Redux reducers and actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,6 +26,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             console.log('Current State is: ' + JSON.stringify(values));
             alert('Current State is: ' + JSON.stringify(values));
             this.toggleModal();
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render() {
@@ -105,7 +106,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             );
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if (comments != null) {
                 return (
                     <div>
@@ -122,7 +123,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                                 </ul>
                             );                        
                         })}
-                        <CommentForm/>
+                        <CommentForm dishId={dishId} addComment={addComment} />
                     </div>
                 );
             } 
@@ -151,7 +152,10 @@ const minLength = (len) => (val) => val && (val.length >= len);
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id}
+                    />
                     </div>
                 </div>
             </div>
